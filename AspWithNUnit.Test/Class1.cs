41973eb26e641b80af704af63f9800caa596b658
srsave <srsave@gmail.com>
2021-03-19 13:21:37 +0530

    k

---
+++ AspWithNUnit.Test/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using System.Web;
+using System;
+using System.Text.RegularExpressions;
+
+//using System.Net.Mail;
+using System.Configuration;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using AspWithNUnit;
+namespace SampleAddition1_NUnit
+{
+    //TestSet
+    public class TetstSetInfo
+    {
+
+        public string l_Num1 { get; set; }
+        public string l_Num2 { get; set; }
+
+        public TetstSetInfo(string _Num1, string _Num2)
+        {
+            l_Num1 = _Num1;
+            l_Num2 = _Num2;
+
+        }
+
+    }
+
+    [TestFixture]
+    public class TestCases
+    {
+        private static IEnumerable<TetstSetInfo> ParamterValuesSet1
+        {
+            get
+            {
+
+                yield return new TetstSetInfo("1", "10");
+                yield return new TetstSetInfo("500", "1000");
+                yield return new TetstSetInfo("50.70", "30");
+            }
+        }
+        private static IEnumerable<TetstSetInfo> ParamterValuesSet2
+        {
+            get
+            {
+
+                yield return new TetstSetInfo("", "10");
+                yield return new TetstSetInfo("500", "");
+                yield return new TetstSetInfo("NN", "10.800");
+                yield return new TetstSetInfo("10.800", "20");
+            }
+        }
+
+        [Test, TestCaseSource("ParamterValuesSet1")]
+        public void checkAddition(TetstSetInfo tci)
+        {
+            AddClass add1 = new AddClass();
+            string valMsg = "";
+            string valAdd = "";
+            bool flag = add1.addition(tci.l_Num1, tci.l_Num2, out valMsg, out valAdd);
+            // Assert.AreEqual(true, flag);
+
+            if (flag == true)
+            {
+                Assert.Pass("Passed Positive Test Case1");
+            }
+            else
+            {
+                Assert.Fail("Failed Positive Test Case1");
+            }
+
+        }
+        [Test, TestCaseSource("ParamterValuesSet2")]
+        public void checkAddition1(TetstSetInfo tci)
+        {
+            AddClass add1 = new AddClass();
+            string valMsg = "";
+            string valAdd = "";
+            bool flag = add1.addition(tci.l_Num1, tci.l_Num2, out valMsg, out valAdd);
+            // Assert.AreEqual(true, flag);
+
+            if (flag == false)
+            {
+                Assert.Pass("Passed Validation : " + valMsg);
+            }
+            else
+            {
+                Assert.Pass("Failed Validation : " + valMsg);
+            }
+
+        }
+    }
+}
---
+++ AspWithNUnit/obj/Debug/Package/PackageTmp/Addition.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Addition.aspx.cs" Inherits="AspWithNUnit.Addition" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table border="1">
+                <tr>
+                    <td>Num1</td>
+                    <td>
+                        <asp:TextBox ID="txtnum1" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Num2</td>
+                    <td><asp:TextBox ID="txtNum2" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" /></td>
+                    
+                </tr>
+                <tr>
+                    <td>Addition</td>
+                    <td>
+                        <asp:Label ID="lblAdd" runat="server" Text="Label"></asp:Label>
+                        <asp:Label ID="lblError" runat="server" Text="" ForeColor="Red"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
